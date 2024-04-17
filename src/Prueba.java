5da6cff9950b71ac3925931a77124cef2d94276d
rsanmarti <rsanmarti@educa.jcyl.es>
2024-04-17 09:46:09 +0200

    pru 1

---
+++ src/Prueba.java
@@ -0,0 +1,14 @@
+
+public class Prueba {
+
+	public Prueba() {
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		System.out.println("hola mundo");
+	}
+
+}
