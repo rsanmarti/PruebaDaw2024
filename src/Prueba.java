fa09ecdfaadf7820281c0b9c37355b0684486e57
rsanmarti <rsanmarti@educa.jcyl.es>
2024-04-17 09:53:03 +0200

    erer

--- src/Prueba.java
+++ src/Prueba.java
@@ -8,7 +8,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 
-		System.out.println("hola mundo 4");
+		System.out.println("hola mundo 33
+5");
 	}
 
 }
