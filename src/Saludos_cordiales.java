a433f93e62c88f1bf7db5bda6d4e1bcb5d94ca78
SoniaMarCob <sonia.marcob@educa.jcyl.es>
2024-04-18 18:35:55 +0200

    Actualizado

--- src/Saludos_cordiales.java
+++ src/Saludos_cordiales.java
@@ -2,8 +2,7 @@
 public class Saludos_cordiales {
 
 	public static void main(String[] args) {
-		System.out.println("Saludos cordiales majos");
-
+		System.out.println("Saludos cordiales, majos");
+		System.out.println("¿Cómo estáis?");
 	}
-
 }
